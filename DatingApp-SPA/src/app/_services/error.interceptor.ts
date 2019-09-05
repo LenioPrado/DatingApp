c42aba1fd927a66a311a7455d02deafa0b1bf771
LenioPrado <lenio.junior@gmail.com>
2019-09-05 15:09:30 -0300

    End of Section 10

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -31,7 +31,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                 return throwError(modalStateErrors || serverError || 'Server Error');
             }
         })
-    )
+    );
   }
 }
 
